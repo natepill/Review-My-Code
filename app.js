47d24a54b95b408582124b31d1194c336b1f7aba
Nathan Pillai <natepill@gmail.com>
2018-08-31 10:30:20 -0700

    Users can see all messages

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+// INDEX
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
+
+
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
+
+
 
 
 app.listen(3000, () => {
