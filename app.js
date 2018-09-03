d7d859cb5421d1ce4360d818e2dfd2a29b4a5e73
Nathan Pillai <natepill@gmail.com>
2018-09-03 10:58:26 -0700

    Users can now create Reviews

--- app.js
+++ app.js
@@ -2,22 +2,30 @@ const express = require ('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String
+});
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" },
-  { title: "Nathan's Adventure!"}
-]
-
-// INDEX
-app.get('/reviews', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
-})
-
-
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Next Review" },
+//   { title: "Nathan's Adventure!"}
+// ]
 
 app.get('/', (req, res) => {
   Review.find()
@@ -30,15 +38,24 @@ app.get('/', (req, res) => {
 })
 
 
+// INDEX
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+})
 
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
-
-const Review = mongoose.model('Review', {
-  title: String
-});
-
+//CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
 
 
 
