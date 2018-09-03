f7f84dea7d44362ef6c35ae39d46e07e49cfd8d9
Nathan Pillai <natepill@gmail.com>
2018-09-03 13:29:05 -0700

    Users can create single review pages

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: Number
 });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -51,12 +52,20 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect(`/reviews/${review._id}`);
     }).catch((err) => {
         console.log(err.message);
     })
 })
 
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+      res.render('reviews-show', {review: review})
+  }).catch((err) =>{
+      console.log(err.message);
+  })
+})
 
 
 app.listen(3000, () => {
